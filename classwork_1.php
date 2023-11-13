93237ac826386d045e00e748498b82fa36bd5e81
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-11-13 10:34:40 +0100

    Complete Exercise 1

---
+++ classwork_1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <title>Classwork 1</title>
+</head>
+<body>
+  <div class="container mt-5">
+  <!-- ---------- -->
+  <h1>Exercise 1</h1>
+  <!-- ---------- -->
+<form action="classwork_1.php" method="GET" class="row row-cols-2">
+
+  <label for="firstname" class="col-4 my-2">Enter your firstname</label>
+  <input type="text" name="firstname" id="firstname" class="col-8 my-2">
+
+  <label for="lastname" class="col-4 my-2">Enter your lastname</label>
+  <input type="text" name="lastname" id="lastname" class="col-8 my-2">
+
+  <input type="submit" name="submit" class="btn btn-outline-light col-4 my-2">
+
+</form>
+
+  <?php
+if(isset($_GET['submit']))
+{
+  if($_GET["firstname"] || $_GET["lastname"])
+  {
+    echo "<h4 class='my-3'>Welcome {$_GET["firstname"]} {$_GET["lastname"]}!</h4>";
+  }
+}
+  ?>
+  </div>
+</body>
+</html>
---
+++ submit.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <title>Classwork 1</title>
+</head>
+<body>
+  <div class="container mt-5">
+
+  <h1>Thank you for your submition!</h1>
+  <a href="classwork_1.php" class="btn btn-outline-light">Go back</a>
+
+
+  </div>
+</body>
+</html>
